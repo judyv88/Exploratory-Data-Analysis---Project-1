21e0ccd45787fd236557830cccecb2a77c63fe79
judyv88 <32971933+judyv88@users.noreply.github.com>
2018-03-18 00:39:12 +0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,21 @@
+#Load the dataset
+rm(list = ls())
+dataset <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
+
+dataset$Date <- as.Date(dataset$Date, format = "%d/%m/%Y")
+
+dataset <- subset(dataset, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+dataset$datetime <- strptime(paste(dataset$Date, dataset$Time), "%Y-%m-%d %H:%M:%S")
+
+#Plot 3
+dataset$datetime <- as.POSIXct(dataset$datetime)
+attach(dataset)
+plot(Sub_metering_1 ~ datetime, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(Sub_metering_2 ~ datetime, col = "Red")
+lines(Sub_metering_3 ~ datetime, col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"),
++ legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
+detach(dataset)
